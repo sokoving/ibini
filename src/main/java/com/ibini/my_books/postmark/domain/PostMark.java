066cd794d5074b00b23b97645c92cc912ffcf1ba
jyeonki <kay8825@naver.com>
2022-08-09 10:34:59 +0900

    [22.08.09 지연] PostMark, PostMemo domain 완료, repository 진행중

---
+++ src/main/java/com/ibini/my_books/postmark/domain/PostMark.java
@@ -0,0 +1,18 @@
+package com.ibini.my_books.postmark.domain;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter @Setter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostMark {
+
+    private Long markNo;    // 북마크 번호
+    private Long postNo;    // 포스트 번호
+    private Long episodeNo; // 에피소드 (회차, 페이지
+    private String content; // 내용
+    private Date regDate;   // 작성일자
+    private Date mod_date;  // 수정일자
+}
---
+++ src/main/java/com/ibini/my_books/postmark/repository/PostMarkMapper.java
@@ -0,0 +1,14 @@
+package com.ibini.my_books.postmark.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PostMarkMapper {
+
+    // 마크 저장 기능
+    // 마크 수정 기능
+    // 마크 삭제 기능
+    // 마크 전체 조회 기능
+    // 마크 개별 조회 기능
+    // 전체 마크 수 구하는 기능
+}
