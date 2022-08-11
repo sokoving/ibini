d04da54e3d30b2f76ef6139644f93d4b92cf0d9d
sokoving <sokoving@gmail.com>
2022-08-11 18:21:53 +0900

    [22.08.10 여운] post-reg.jsp 작업중

---
+++ src/main/resources/static/js/post-reg.js
@@ -0,0 +1,11 @@
+// -------------- 전역변수, 함수 선언부 ----------- //
+function jQueryTest(msg){
+    const $test = $('h1');
+    console.log($test + " / "  +  msg );
+    }
+
+
+function 
+
+// ------------ 함수 실행부 ------------ //
+// jQueryTest("post-reg.js");
