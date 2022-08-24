a7e1d8d36a39fff73babcb62c3e5ed435e171bb7
jyeonki <kay8825@naver.com>
2022-08-24 16:02:01 +0900

    [22.08.24 지연] PostMark, PostMemo domain, Mapper.xml 추가 및 수정

--- src/main/java/com/ibini/my_books/postnote/domain/PostMark.java
+++ src/main/java/com/ibini/my_books/postnote/domain/PostMark.java
@@ -13,8 +13,8 @@ public class PostMark {
     private Long postNo;         // 포스트 번호
     private Long episodeNo;      // 에피소드 (회차, 페이지, 권, %)
     private String content;      // 내용
-    private Date regDateTime;    // 작성일자
-    private Date modDateTime;    // 수정일자
+    private Date regDateTime;    // 작성일자, 시간
+    private Date modDateTime;    // 수정일자, 시간
     private String regAccount;   // 작성 account
     private String modAccount;   // 수정 account
 }
