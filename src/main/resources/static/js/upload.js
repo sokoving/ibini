446bfeb46a919b04146a49850fc978f829b035dc
sokoving <sokoving@gmail.com>
2022-09-01 08:32:09 +0900

    [22.09.01 여운] 썸네일 중복업로드 수정(첨부이미지 수정 예정)

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -3,28 +3,44 @@ function showThumbImg(fileName, fileOriginName) {
     console.log('showThumbImg 함수 호출 : ', fileName);
     console.log('showThumbImg 함수 호출 : ', fileOriginName);
 
-    // if($('#reg-1 .box-msg').hasClass('hide')){
-    //     console.log("이미 등록돼 있는 썸네일, 히든인풋 지우고 다시 넣기");
-    // }
-
     // 썸네일 랜더링
-    $('#reg-1 .box-msg').addClass('hide')
-    console.log($('#reg-1 .box-msg'));
+    const $thumbImg = $('.thumb-img');
+    console.log($thumbImg.length);
+    // 등록된 썸네일이 없을 때
+    if ($thumbImg.length <= 0) {
+        $('#reg-1 .box-msg').addClass('hide');
 
-    const $img = document.createElement('img');
-    $img.classList.add('post-img');
-    $img.setAttribute('src', '/loadFile?fileName=' + fileName);
-    $img.setAttribute('alt', fileOriginName);
-    $('#reg-1 .img-box').append($img);
+        // 이미지 랜더링
+        const $img = document.createElement('img');
+        $img.classList.add('post-img');
+        $img.classList.add('thumb-img');
+        $img.setAttribute('src', '/loadFile?fileName=' + fileName);
+        $img.setAttribute('alt', fileOriginName);
+        $('#reg-1 .img-box').append($img);
 
-    $('#reg-1 .file-box-right').text(fileOriginName)
-
-    // 등록 때 다 같이 날릴 것
+    // 등록 때 썸네일 경로 보내는 히든인풋
     const $thumbFileName = document.createElement('input');
+    $thumbFileName.classList.add('thumbFileName-hidden-input');
     $thumbFileName.setAttribute('type', 'hidden');
     $thumbFileName.setAttribute('name', 'thumbFileName');
     $thumbFileName.setAttribute('value', fileName);
     $('#write-form').append($thumbFileName);
+
+        // 등록된 썸네일이 있을 때
+    } else {
+
+        // 이미지 주소 교체
+        $thumbImg.attr('src', '/loadFile?fileName=' + fileName);
+        $thumbImg.attr('alt', fileOriginName);
+
+        // 등록 때 썸네일 경로 보내는 히든인풋
+        $('.thumbFileName-hidden-input').attr('value', fileName);
+    }
+
+    // 박스에 파일이름 띄우기
+    $('#reg-1 .file-box-right').text(fileOriginName)
+
+
 }
 
 
@@ -33,9 +49,9 @@ function showImgs(fileNames) {
     console.log('showImgs 함수 호출 : ', fileNames);
     const $boxMsg = $('#reg-6 .file-box-right');
     // console.log($boxMsg.data(imgsNum));
-    
+
     for (const fileName of fileNames) {
-        
+
         // 랜더링
         const $imgBox = document.createElement('div');
         $imgBox.classList.add('upload-img-box');
