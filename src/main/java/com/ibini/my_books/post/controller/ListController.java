a21fb67d87580e074f091bf07d22b656831cb203
sokoving <sokoving@gmail.com>
2022-09-04 21:04:22 +0900

    [22.09.04 여운] detail-example.html 연관포스트 관련 css, js 초기 작업

--- src/main/java/com/ibini/my_books/post/controller/ListController.java
+++ src/main/java/com/ibini/my_books/post/controller/ListController.java
@@ -3,17 +3,22 @@ package com.ibini.my_books.post.controller;
 import com.ibini.my_books.genre.service.GenreService;
 import com.ibini.my_books.hashtag.service.HashTagService;
 import com.ibini.my_books.platform.service.PlatformService;
+import com.ibini.my_books.post.dto.PostWithName;
 import com.ibini.my_books.post.service.PostService;
 import com.ibini.my_books.postImg.service.PostImgService;
 import com.ibini.my_books.util.LoginUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @Log4j2
@@ -30,7 +35,7 @@ public class ListController {
 
     @GetMapping()
     public String postList(Model model, HttpSession session) {
-        log.info("ListController /list  GET 요청!!");
+        log.info("ListController /list  GET!!");
 
         String account = LoginUtil.getCurrentMemberAccountForDB(session);
 
@@ -39,6 +44,18 @@ public class ListController {
         model.addAttribute("pfl", platformService.findAllPlatform(account));
 
         return "post/post-list";
+    }
+    //    포스트 전체 리스트를 비동기로 보내줌
+    @GetMapping("/api/allPost")
+    @ResponseBody
+    public ResponseEntity<List<PostWithName>> getApiList(
+//            HttpSession session
+    ){
+        log.info("ListController /api/allPost GET!!");
+//        String account = LoginUtil.getCurrentMemberAccountForDB(session);
+        String account = "2209030021";
+        List<PostWithName> postList = postService.finaAllPostWithNameService(account);
+        return new ResponseEntity<>(postList, HttpStatus.OK);
     }
 
 }
