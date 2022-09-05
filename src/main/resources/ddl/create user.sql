cff0a9eecedd77cff87d320309f729e899a5f0ca
sokoving <sokoving@gmail.com>
2022-09-05 16:38:37 +0900

    [22.09.05 여운] 재영 MARIADB 병합

--- src/main/resources/ddl/create user.sql
+++ src/main/resources/ddl/create user.sql
@@ -1,3 +1,27 @@
+-------------- Maria DB ----------------
+------ cmd로 ibini database 만들기 ------
+-- 폴더 이동
+cd "C:\Program Files\MariaDB 10.7\bin"
+-- mysql 구동, 패스워드 입력
+Enter password: mariadb
+-- 성공 메세지
+Welcome to the MariaDb monitor.
+-- ibini DATABASE 만들기
+create database ibini;
+-- 생성 확인 (ibini 확인)
+show databases;
+
+------ cmd로 root 패스워드 변경 ------
+-- mysql 구동, 패스워드 입력 후\
+-- 데이터베이스를 mysql로 변경
+use mysql
+-- 비밀번호 변경
+MariaDB [mysql]> set password=password('mariadb');
+-- 종료
+exit;
+
+
+--------------- 오라클--------------------
 -- 1. admin 계정에서 ibini 계정 생성, 권한 부여
 CREATE USER ibini IDENTIFIED BY 1234;
 GRANT CONNECT, RESOURCE, DBA TO ibini;
