d5a24953c6245f58c520c7ff307bd6d9adce34dc
jyeonki <kay8825@naver.com>
2022-09-05 11:32:24 +0900

    [22.09.02 지연] PostMarkMapper.xml, PostMemoMapper.xml MariaDB로 변환 PostMark, PostMemo domain 수정, ddl(prj_post_mark, prj_post_memo 추가)

--- src/main/java/com/ibini/my_books/postnote/domain/PostMemo.java
+++ src/main/java/com/ibini/my_books/postnote/domain/PostMemo.java
@@ -14,8 +14,7 @@ public class PostMemo {
     private String content;      // 내용
     private Date regDatetime;    // 작성일자, 시간
     private Date modDatetime;    // 수정일자, 시간
-    private String regAccount;   // 작성 account
-    private String modAccount;   // 수정 account
+    private String account;      // 작성 account
 
     //커스텀 필드
     private String PrettierDate;
